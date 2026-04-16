4cf712ef0353b67329d66978b0797241be956fc1
Dadinaut <dadinaut@gmail.com>
2026-04-16 19:20:37 +0200

    faq section added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const closeSidebarButton = document.querySelector(
   ".links-container-wrapper .close-icon",
 );
 const sidebar = document.querySelector(".links-container-wrapper");
+const faqShowButtons = document.querySelectorAll(
+  ".question-response-row .show-button",
+);
 
 closeSidebarButton.addEventListener("click", () => {
   sidebar.classList.remove("show-sidebar");
@@ -23,5 +26,23 @@ const observer = new ResizeObserver(([entry]) => {
     body.dataset.onLargeScreen = false;
   }
 });
+// Every show response button in faq has a response paragraph associated with the data-linkId, a paragraph with the same value for data-linkId. When we click on a button, we show or hide the response paragraph associated.
+for (const button of faqShowButtons) {
+  const response = document.querySelector(
+    `p[data-linkId=${button.dataset.linkid}]`,
+  );
+
+  button.addEventListener("click", () => {
+    if (button.dataset.showresponse === "false") {
+      response.style.display = "block";
+      button.dataset.showresponse = "true";
+      button.textContent = "Fermer";
+    } else {
+      response.style.display = "none";
+      button.dataset.showresponse = "false";
+      button.textContent = "Ouvrir";
+    }
+  });
+}
 
 observer.observe(body);
